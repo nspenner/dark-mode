55115299b29b392566aab8c16042ec2f328fdfed
Nathan Minchow <nathan.minchow@outlook.com>
2018-10-29 01:34:35 -0500

    init

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleDarkMode() {
+  if (document.body.style.cssText.includes("rgba(0, 0, 0, 0.75)")) {
+    document.body.style.setProperty("--color2", "rgba(72, 169, 166, 1)");
+    document.body.style.setProperty("--color3", "rgba(228, 223, 218, 1)");
+  }
+  else {
+    document.body.style.setProperty("--color2", "rgba(0, 0, 0, 0.75)");
+    document.body.style.setProperty("--color3", "rgba(0, 0, 0, 0.5)")
+    console.log(document.body.style);
+  }
+}
